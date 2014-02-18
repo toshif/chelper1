3aea35571a640e1bd3e96346f7f346cce1ad21a0
toshif <toshif>
2014-02-18 21:54:27 +0900

    de

--- src/fuku/TaskD.java
+++
@@ -1,9 +0,0 @@
-package fuku;
-
-import java.io.PrintWriter;
-import java.util.Scanner;
-
-public class TaskD {
-    public void solve(int testNumber, Scanner in, PrintWriter out) {
-    }
-}
--- src/fuku/TaskE.java
+++
@@ -1,9 +0,0 @@
-package fuku;
-
-import java.io.PrintWriter;
-import java.util.Scanner;
-
-public class TaskE {
-    public void solve(int testNumber, Scanner in, PrintWriter out) {
-    }
-}
