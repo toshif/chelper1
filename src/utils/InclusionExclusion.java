a61322ad7911ad9c90e746b799f16a27540a25e6
toshif <tfukuzawa@live.jp>
2014-05-20 22:05:49 +0900

    cp

--- src/utils/InclusionExclusion.java
+++ src/utils/InclusionExclusion.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 含除原理を使った計算
  *
+ * 下の例は, X以下の整数で、a1, a2, .. ( M 個 ) のうち少なくとも1つで割り切れるものの個数の計算.
+ *
+ * --- で囲まれた部分が、この問題に特有の部分。
  *
  * http://en.wikipedia.org/wiki/Inclusion%E2%80%93exclusion_principle
  *
@@ -15,6 +19,8 @@ public class InclusionExclusion {
     static class IeSet {
         int a = 0;
     }
+
+    int X = 100;
 
     // the number of sets
     int M = 2;
@@ -39,7 +45,13 @@ public class InclusionExclusion {
 
             // DO calculate AND of the andSets
             // A ∩ B ∩ C ..
-            long andResult = 0;
+            //----------------------------
+            long lcm = 1;
+            for (IeSet s : andSets) {
+                lcm = GCD.lcm(s.a, lcm);
+            }
+            long andResult = X / lcm;
+            //----------------------------
 
             tot += sign * andResult;
         }
@@ -55,7 +67,7 @@ public class InclusionExclusion {
         ie.sets[0] = s1;
 
         IeSet s2 = new IeSet();
-        s2.a = 2;
+        s2.a = 3;
         ie.sets[1] = s2;
 
         long ret = ie.doInclusionExclusion();
