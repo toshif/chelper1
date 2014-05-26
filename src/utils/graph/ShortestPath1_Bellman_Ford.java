b09d65d85f3190d0de29de1610cdcfb08698a9b6
toshif <tfukuzawa@live.jp>
2014-05-26 21:55:54 +0900

    cp

---
+++ src/utils/graph/ShortestPath1_Bellman_Ford.java
@@ -0,0 +1,21 @@
+package utils.graph;
+
+/**
+ * Created by toshif on 5/20/14.
+ */
+public class ShortestPath1_Bellman_Ford {
+
+    static int N = 10;
+
+    static int[][] g = new int[N][N];
+
+    static void bellmanFord(){
+
+    }
+
+    public static void main(String[] args){
+        String costs = "A,B,2 B,C,4, A,C,5";
+
+    }
+
+}
---
+++ src/utils/matrix/GaussJordanElimination.java
@@ -0,0 +1,116 @@
+package utils.matrix;
+
+import java.util.Arrays;
+
+/**
+ * Gauss Jordan の消去法。
+ *
+ * 連立一次方程式を解くのに使われる。
+ *
+ * http://en.wikipedia.org/wiki/Gaussian_elimination
+ *
+ * Created by toshif on 5/26/14.
+ */
+public class GaussJordanElimination {
+
+    final double EPS = 1E-8;
+
+    int N = 3;
+
+    /**
+     * A x = b を解く(ベクトル x を求める)。
+     *
+     * (Aは正方行列、b はベクトル)
+     * の A と b をつなげたものを B とする。
+     */
+    double[][] B = new double[N][N+1];
+
+    double[] gaussJordan(){
+        // i 番目の column の係数を 1 にし、それ以外の式から引いて係数をゼロにする
+        for (int i = 0; i < N; i++) {
+            printB();
+
+            // 絶対値の大きい
+            int pivot = i;
+            for (int j = i; j < N; j++) {
+                if (Math.abs(B[j][i]) > Math.abs(B[pivot][i])) pivot = j;
+            }
+
+            swap(pivot, i);
+
+            // 解なし / 一意でない
+            if (Math.abs(B[i][i]) < EPS) return null;
+
+            // 係数を1にする
+            for (int j = i + 1; j < N + 1; j++) B[i][j] /= B[i][i];
+            B[i][i] = 1.0;
+
+            // column i を row i 以外 係数ゼロにする
+            for (int j = 0; j < N; j++) {
+                double c = B[j][i];
+                if (i != j) {
+                    for (int k = 0; k < N + 1; k++) {
+                       B[j][k] -= c * B[i][k];
+                    }
+                }
+            }
+        }
+
+        // solved
+        printB();
+
+        double[] ans = new double[N];
+        for (int i = 0; i < N; i++) {
+            ans[i] = B[i][N];
+        }
+
+        return ans;
+    }
+
+    /** swap the row i and row j
+     */
+    void swap(int i, int j){
+        double[] tmp = B[i];
+        B[i] = B[j];
+        B[j] = tmp;
+    }
+
+    void printB(){
+        for (int j = 0; j < N; j++) {
+            System.out.printf("B[%d] = %s\n", j, Arrays.toString(B[j]));
+        }
+        System.out.println("----");
+    }
+
+    void init(){
+        /**
+         * <code>
+         *   x  - 2y + 3z  = 6
+         *   4x - 5y + 6z  = 12
+         *   7x - 8y + 10z = 21
+         * </code>
+         */
+        B[0] = new double[]{1, -2, 3, 6};
+        B[1] = new double[]{4, -5, 6, 12};
+        B[2] = new double[]{7, -8, 10, 21};
+    }
+
+    static int[] getIntArray(double[] darray){
+        int[] ret = new int[darray.length];
+        for (int i = 0; i < darray.length; i++) {
+            // by default, it rounds to the closest long
+            ret[i] = (int) Math.round(darray[i]);
+        }
+        return ret;
+    }
+
+    public static void main(String[] args){
+        GaussJordanElimination gj = new GaussJordanElimination();
+        gj.init();
+        double[] ans = gj.gaussJordan();
+
+        System.out.println("answer = " + Arrays.toString(ans));
+        System.out.println("answer in int = " + Arrays.toString(getIntArray(ans)));
+    }
+
+}
