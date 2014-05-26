a2bab1f0b88eeb74ffea57fbd6a208c102126f82
toshif <tfukuzawa@live.jp>
2014-05-26 22:06:45 +0900

    cp

--- src/utils/matrix/GaussJordanElimination.java
+++ src/utils/matrix/GaussJordanElimination.java
@@ -25,6 +25,10 @@ public class GaussJordanElimination {
      */
     double[][] B = new double[N][N+1];
 
+    // 解が一意
+    // 一意でないとき false をセット
+    boolean isUnique = true;
+
     double[] gaussJordan(){
         // i 番目の column の係数を 1 にし、それ以外の式から引いて係数をゼロにする
         for (int i = 0; i < N; i++) {
@@ -38,8 +42,12 @@ public class GaussJordanElimination {
 
             swap(pivot, i);
 
+            // row i 以降の全ての row で column i = 0
             // 解なし / 一意でない
-            if (Math.abs(B[i][i]) < EPS) return null;
+            if (Math.abs(B[i][i]) < EPS){
+                isUnique = false;
+                continue;
+            }
 
             // 係数を1にする
             for (int j = i + 1; j < N + 1; j++) B[i][j] /= B[i][i];
@@ -56,8 +64,11 @@ public class GaussJordanElimination {
             }
         }
 
-        // solved
+        // done
         printB();
+
+        // 解なし / 一意でない
+        if (!isUnique) return null;
 
         double[] ans = new double[N];
         for (int i = 0; i < N; i++) {
@@ -82,7 +93,7 @@ public class GaussJordanElimination {
         System.out.println("----");
     }
 
-    void init(){
+    void init1(){
         /**
          * <code>
          *   x  - 2y + 3z  = 6
@@ -95,6 +106,20 @@ public class GaussJordanElimination {
         B[2] = new double[]{7, -8, 10, 21};
     }
 
+    void init2(){
+        // 解が一意でない
+        /**
+         * <code>
+         *   x  - 2y + 3z  = 6
+         *   4x - 5y + 6z  = 12
+         *   4x - 5y + 6z = 12
+         * </code>
+         */
+        B[0] = new double[]{1, -2, 3, 6};
+        B[1] = new double[]{4, -5, 6, 12};
+        B[2] = new double[]{4, -5, 6, 12};
+    }
+
     static int[] getIntArray(double[] darray){
         int[] ret = new int[darray.length];
         for (int i = 0; i < darray.length; i++) {
@@ -105,12 +130,21 @@ public class GaussJordanElimination {
     }
 
     public static void main(String[] args){
+        System.out.println("---------- init1 -----------");
+
         GaussJordanElimination gj = new GaussJordanElimination();
-        gj.init();
+        gj.init1();
         double[] ans = gj.gaussJordan();
 
         System.out.println("answer = " + Arrays.toString(ans));
         System.out.println("answer in int = " + Arrays.toString(getIntArray(ans)));
+
+        System.out.println("---------- init2 -----------");
+
+        gj.init2();
+        ans = gj.gaussJordan();
+
+        System.out.println("answer = " + Arrays.toString(ans));
     }
 
 }
