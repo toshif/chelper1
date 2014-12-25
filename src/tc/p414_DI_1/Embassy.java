e32a83f6a3f6566760203408f019ffadfeaf695b
toshif <tfukuzawa@live.jp>
2014-12-25 22:25:22 +0900

    note

--- src/tc/p414_DI_1/Embassy.java
+++ src/tc/p414_DI_1/Embassy.java
@@ -4,7 +4,29 @@ package tc.p414_DI_1;
 public class Embassy {
 
     int visaApplication(int[] forms, int dayLength, int openTime){
-        return 0;
+        int N = forms.length;
+        int mi = 1000_000_000;
+        int offset = 0;
+        for (int i = 0; i < N; i++) {
+            offset = forms[i];
+
+            if (offset > forms[0]) continue;
+
+            int time = -offset;
+            for (int j = 0; j < N; j++) {
+                time += forms[j];
+                int timeInDay = time % dayLength;
+                if (timeInDay <= openTime) continue;
+
+                // if alreay closed, wait for the next day
+                time += (dayLength - timeInDay);
+            }
+
+            int tot = offset + time;
+            mi = Math.min(mi, tot);
+        }
+
+        return mi;
     }
 
 }
