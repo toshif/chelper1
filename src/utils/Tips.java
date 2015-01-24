d00e5dec399ce7f82cc6aaffa7f5f3f265b4ba40
toshif <tfukuzawa@live.jp>
2015-01-25 04:41:59 +0900

    desk

--- src/utils/Tips.java
+++ src/utils/Tips.java
@@ -6,18 +6,34 @@ package utils;
 public class Tips {
 
     public static void main(String[] args) {
-        reversingString();
+        //reversingString();
+        roundUpDivide();
     }
 
     /**
      * Reverse a string : "hello" -> "olleh"
      */
-    static void reversingString(){
+    static void reversingString() {
         String s = "hello";
 
         String reversed = new StringBuilder(s).reverse().toString();
 
         System.out.println("reversed = " + reversed);
+    }
+
+    /**
+     * round up the integer diviion
+     *
+     *
+     * <p/>
+     * a = 5, b = 3
+     * round down : a/b = 1
+     * round up : a/b = 2
+     *
+     * how : (a + (b-1)) / b
+     */
+    static int roundUpDivide(int a, int b) {
+        return (a + b - 1) / b;
     }
 
 }
