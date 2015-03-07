e27c26f870c1b3aa6250452da0e564c2f2664b22
toshif <tfukuzawa@live.jp>
2015-03-07 11:47:51 +0900

    desk

---
+++ src/template/atcoder/p3/Main.java
@@ -0,0 +1,26 @@
+package template.atcoder.p3;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        new Solver().solve(in, out);
+        out.flush();
+    }
+
+    private static class Solver {
+        public void solve(Scanner in, PrintWriter out) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int c = in.nextInt();
+            in.nextLine();
+            String s = in.nextLine();
+            out.printf("%d %s\n", a+b+c, s);
+        }
+    }
+
+}
