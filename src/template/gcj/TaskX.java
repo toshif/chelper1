3f3cb7df41e07c2487177b9b2c5f70c6b21f866c
toshif <tfukuzawa@live.jp>
2015-05-02 21:56:17 +0900

    note

--- src/template/gcj/TaskX.java
+++ src/template/gcj/TaskX.java
@@ -18,17 +18,18 @@ public class TaskX {
         String outFile = inFile + ".out";
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(outFile)));
 
-        System.out.printf("Input File = [%s]\n", inFileUrl);
-        System.out.printf("Output File = [%s]\n", outFile);
+        System.err.printf("Class = [%s]\n", TaskX.class);
+        System.err.printf("Input File = [%s]\n", inFileUrl);
+        System.err.printf("Output File = [%s]\n", outFile);
 
         int T = in.nextInt();
         for (int i = 0; i < T; i++) {
-            System.out.println("------------------");
-            System.out.println("test " + (i+1));
+            System.err.println("------------------");
+            System.err.println("test " + (i+1));
             Solver solver = new Solver();
             String ans = solver.solve(i, in, out);
             out.println("Case #" + (i+1) + ": " + ans);
-            System.out.println("Case #" + (i+1) + ": " + ans);
+            System.err.println("Case #" + (i+1) + ": " + ans);
         }
         out.close();
     }
