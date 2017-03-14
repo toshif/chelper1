9813b484e9cf96020eadd26c41693c69d1ec633d
toshif <tfukuzawa@live.jp>
2017-03-14 16:35:46 +0000

    mac

--- src/utils/graph/ShortestPath2_Dijkstra.java
+++ src/utils/graph/ShortestPath2_Dijkstra.java
@@ -22,7 +22,7 @@ public class ShortestPath2_Dijkstra {
         // 上の　Dijkstra　は Directed graph にも対応している
         String input = "A,B,2 A,C,5 B,C,4 B,D,6 B,E,10 C,D,2 D,F,1 E,F,3 E,G,5 F,G,9";
 
-        Dijkstra dj = new Dijkstra(7);
+        ShortestPath2_Dijkstra dj = new ShortestPath2_Dijkstra(7);
 
         // 存在する path の cost 入力
         String[] costE = input.split(" ");
@@ -45,45 +45,45 @@ public class ShortestPath2_Dijkstra {
         System.out.printf("dijkstra d = %s\n", Arrays.toString(dj.d));
     }
 
-    private static class Dijkstra {
-        static final int INF = 1_000_000_000;
-        int V;
-        int[][] cost;
+    private static final long INF = 1_000_000_000_000_000_000L;
 
-        // 各頂点への最短距離
-        int[] d;
+    private int V;
 
-        Dijkstra(int V) {
-            this.V = V;
-            cost = new int[V][V];
-            d = new int[V];
+    private long[][] cost;
 
-            // 存在しない path は INF
-            for (int i = 0; i < cost.length; i++) {
-                Arrays.fill(cost[i], INF);
-            }
+    // 各頂点への最短距離
+    public long[] d;
 
-            Arrays.fill(d, INF);
+    public ShortestPath2_Dijkstra(int V) {
+        this.V = V;
+        cost = new long[V][V];
+        d = new long[V];
+
+        // 存在しない path は INF
+        for (int i = 0; i < cost.length; i++) {
+            Arrays.fill(cost[i], INF);
         }
 
-        void solve(int s) {
-            d[s] = 0;
+        Arrays.fill(d, INF);
+    }
 
-            boolean[] used = new boolean[V];
-            while (true) {
-                int v = -1;
-                // まだ使われてない頂点のうち、sからの距離が最小のものを探す
-                for (int i = 0; i < V; i++) {
-                    if (!used[i] && (v == -1 || d[i] < d[v])) v = i;
-                }
+    public void solve(int s) {
+        d[s] = 0;
 
-                if (v == -1) break;
+        boolean[] used = new boolean[V];
+        while (true) {
+            int v = -1;
+            // まだ使われてない頂点のうち、sからの距離が最小のものを探す
+            for (int i = 0; i < V; i++) {
+                if (!used[i] && (v == -1 || d[i] < d[v])) v = i;
+            }
 
-                used[v] = true;
+            if (v == -1) break;
 
-                for (int i = 0; i < V; i++) {
-                    d[i] = Math.min(d[i], d[v] + cost[v][i]);
-                }
+            used[v] = true;
+
+            for (int i = 0; i < V; i++) {
+                d[i] = Math.min(d[i], d[v] + cost[v][i]);
             }
         }
     }
