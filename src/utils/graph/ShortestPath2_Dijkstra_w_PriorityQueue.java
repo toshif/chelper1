9e01b2c9c7721709b90665ba34bcf2105c5202bc
toshif <tfukuzawa@live.jp>
2017-03-14 16:42:13 +0000

    mac

--- src/utils/graph/ShortestPath2_Dijkstra_w_PriorityQueue.java
+++ src/utils/graph/ShortestPath2_Dijkstra_w_PriorityQueue.java
@@ -8,20 +8,15 @@ import java.util.*;
  * Time Complexity : O(V log2(V)) for most cases. Worst O(V^2)
  * Memory Space Complexity : O(E + V)
  *
- * <p/>
  * これも極めて密結合な graph の場合、 worst では O(V^2) かかってしまうが、
  * 疎結合の tree ( sparse tree ) で　O(E) ~ O(V) なら、
- * 各 node からの遷移は O(1) なので
- * <p/>
- * O ( V ( 1 + log2(V) ) )　= O (V log2 V) で動く。
- * <p/>
+ * 各 node からの遷移は O(1) なので O ( V ( 1 + log2(V) ) )　= O (V log2 V) で動く。
+ *
  * http://en.wikipedia.org/wiki/Dijkstra's_algorithm
- * <p/>
  * 1. 最短距離が確定した頂点に隣接する頂点を更新する
  * 2. 1 で確定した「最短距離が確定した頂点」はもう使わない
- * <p/>
  * 最短距離が確定した頂点を探すときに Priority Queue を使っている。
- * <p/>
+ *
  * Created by toshif.
  */
 public class ShortestPath2_Dijkstra_w_PriorityQueue {
@@ -31,7 +26,7 @@ public class ShortestPath2_Dijkstra_w_PriorityQueue {
         // 下の　Dijkstra　は Directed graph にも対応している
         String input = "A,B,2 A,C,5 B,C,4 B,D,6 B,E,10 C,D,2 D,F,1 E,F,3 E,G,5 F,G,9";
 
-        Dijkstra dj = new Dijkstra(7);
+        ShortestPath2_Dijkstra_w_PriorityQueue dj = new ShortestPath2_Dijkstra_w_PriorityQueue(7);
 
         // 存在する path の cost 入力
         String[] costE = input.split(" ");
@@ -42,8 +37,8 @@ public class ShortestPath2_Dijkstra_w_PriorityQueue {
             int p1 = c[1].charAt(0) - 'A';
             int cs = Integer.parseInt(c[2]);
 
-            dj.edges[p0].add(new Dijkstra.Edge(p0, p1, cs));
-            dj.edges[p1].add(new Dijkstra.Edge(p1, p0, cs));
+            dj.edges[p0].add(new ShortestPath2_Dijkstra_w_PriorityQueue.Edge(p0, p1, cs));
+            dj.edges[p1].add(new ShortestPath2_Dijkstra_w_PriorityQueue.Edge(p1, p0, cs));
         }
 
         // solve it. find the minimum cost from node 0.
@@ -54,87 +49,86 @@ public class ShortestPath2_Dijkstra_w_PriorityQueue {
         System.out.printf("dk with pq d = %s\n", Arrays.toString(dj.d));
     }
 
-    public static class Dijkstra {
-        static final int INF = 1_000_000_000;
-        int V;
-        public List<Edge>[] edges;
-        public int[] d;
+    private static final long INF = 1_000_000_000_000_000_000L;
 
-        public Dijkstra(int V) {
-            this.V = V;
-            edges = new List[V];
-            d = new int[V];
+    private int V;
 
-            for (int i = 0; i < V; i++) {
-                edges[i] = new ArrayList<>();
-            }
+    public List<Edge>[] edges;
 
-            Arrays.fill(d, INF);
+    public long[] d;
+
+    public ShortestPath2_Dijkstra_w_PriorityQueue(int V) {
+        this.V = V;
+        edges = new List[V];
+        d = new long[V];
+
+        for (int i = 0; i < V; i++) {
+            edges[i] = new ArrayList<>();
         }
 
-        public void solve(int s) {
-            d[s] = 0;
+        Arrays.fill(d, INF);
+    }
 
-            PriorityQueue<Node> queue = new PriorityQueue<>();
-            queue.add(new Node(s, 0));
+    public void solve(int s) {
+        d[s] = 0;
 
-            boolean[] used = new boolean[V];
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        queue.add(new Node(s, 0));
 
-            // loop O(V) times
-            while (!queue.isEmpty()) {
+        boolean[] used = new boolean[V];
 
-                // take O(log2 V)
-                Node node = queue.poll();
-                int v = node.idx;
+        // loop O(V) times
+        while (!queue.isEmpty()) {
 
-                // if already calculated for the vertex, no need to do again
-                if (used[v]) continue;
+            // take O(log2 V)
+            Node node = queue.poll();
+            int v = node.idx;
 
-                used[v] = true;
+            // if already calculated for the vertex, no need to do again
+            if (used[v]) continue;
 
-                // loop O(1) times
-                for (Edge e : edges[v]) {
-                    int to = e.to;
-                    int distance = d[v] + e.cost;
-                    if (distance < d[to]) {
-                        d[to] = distance;
-                        queue.add(new Node(to, distance));
-                    }
+            used[v] = true;
+
+            // loop O(1) times
+            for (Edge e : edges[v]) {
+                int to = e.to;
+                long distance = d[v] + e.cost;
+                if (distance < d[to]) {
+                    d[to] = distance;
+                    queue.add(new Node(to, distance));
                 }
             }
         }
+    }
 
-        static class Node implements Comparable<Node> {
+    private static class Node implements Comparable<Node> {
 
-            int idx;
+        int idx;
 
-            int distance;
+        long distance;
 
-            public Node(int idx, int distance) {
-                this.idx = idx;
-                this.distance = distance;
-            }
-
-            @Override
-            public int compareTo(Node o2) {
-                Node o1 = this;
-
-                if (o1.distance != o2.distance) return o1.distance - o2.distance;
-
-                return o1.idx - o2.idx;
-            }
+        public Node(int idx, long distance) {
+            this.idx = idx;
+            this.distance = distance;
         }
 
-        public static class Edge {
-            int from;
-            int to;
-            int cost;
+        @Override
+        public int compareTo(Node o2) {
+            if (Long.compare(this.distance, o2.distance) != 0) return Long.compare(this.distance, o2.distance);
 
-            public Edge(int from, int to, int cost) {
-                this.from = from;
-                this.to = to;
-                this.cost = cost;
-            }
+            return Integer.compare(this.idx, o2.idx);
+        }
+    }
+
+    public static class Edge {
+        int from;
+        int to;
+        long cost;
+
+        public Edge(int from, int to, long cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
         }
     }
 
