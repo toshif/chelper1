b1699f2337e4f164bf8772cca595cc7a52262d6e
toshif <tfukuzawa@live.jp>
2017-04-19 22:58:07 +0100

    mac

--- gcj_dist/src/template/Main.java
+++ gcj_dist/src/template/Main.java
@@ -2,9 +2,43 @@ package template;
 
 public class Main {
 
+    private final int MASTER_NODE = 0;
+    private int numOfNodes;
+    private int nodeId;
+
+    Main() {
+        nodeId = message.MyNodeId();
+        numOfNodes = message.NumberOfNodes();
+    }
+
+    void doWork( ) {
+        System.err.println("doing work for node");
+        message.PutLL(MASTER_NODE, 1L);
+        message.PutLL(MASTER_NODE, 5L);
+        message.Send(MASTER_NODE);
+    }
+
+    void doMaster() {
+        long ma = 0;
+        for (int i = 0; i < numOfNodes; i++) {
+            message.Receive(i);
+            long lo = message.GetLL(i);
+            long hi = message.GetLL(i);
+            ma = Math.max(ma, hi - lo);
+            System.err.println("ma for node " + i + " = " + ma);
+        }
+        System.out.println(ma);
+    }
+
+    boolean isMaster( ) {
+        return nodeId == MASTER_NODE;
+    }
+
     public static void main(String[] args) {
-        if (message.MyNodeId() == 0) {
-            System.out.println("1");
+        Main m = new Main();
+        m.doWork();
+        if (m.isMaster()) {
+            m.doMaster();
         }
     }
 
