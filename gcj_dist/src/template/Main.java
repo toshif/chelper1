046e00301a8825eeb588caa96e7b222914b569df
toshif <tfukuzawa@live.jp>
2017-04-26 00:35:38 +0100

    mac

--- gcj_dist/src/template/Main.java
+++ gcj_dist/src/template/Main.java
@@ -88,13 +88,19 @@ public class Main {
         }
     }
 
-    // hash
-    long xorshift64star(long x) {
+    // pseudo random
+    static long xorshift64star(long x) {
         x ^= x >> 12;
         x ^= x << 25;
         x ^= x >> 27;
         return x * 0x2545F4914F6CDD1DL;
     }
+
+    // hash : map the given v to a value in [0 .. n-1]
+    static int hash(long v, int n) {
+        return (int) (Math.abs(xorshift64star(v)) % (long) n);
+    }
+
 
     public static void main(String[] args) {
         Main m = new Main();
