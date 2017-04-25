b0861e03a0434ad6b36514d83aab223eaa2d09bf
toshif <tfukuzawa@live.jp>
2017-04-26 00:39:50 +0100

    mac

--- gcj_dist/work/run.sh
+++ gcj_dist/work/run.sh
@@ -15,7 +15,7 @@ for in_file in $INPUTS; do
     rm -f ../tmp/*
 
     echo "----- $in_file -----"
-    cat $TARGET_MAIN_JAVA | egrep -v "package template;" > Main.java
+    cat $TARGET_MAIN_JAVA | egrep -v "^package .+;" > Main.java
     cp ../$in_file $INPUT_JAVA
 
     $DCJ test --source Main.java --library $INPUT_JAVA --nodes $NUM_OF_NODES
