1b8e36e72755b51ae6b5a29da49ec730c1eda67d
toshif <tfukuzawa@live.jp>
2017-04-26 00:18:16 +0100

    mac

--- gcj_dist/src/template/Main.java
+++ gcj_dist/src/template/Main.java
@@ -88,6 +88,14 @@ public class Main {
         }
     }
 
+    // hash
+    long xorshift64star(long x) {
+        x ^= x >> 12;
+        x ^= x << 25;
+        x ^= x >> 27;
+        return x * 0x2545F4914F6CDD1DL;
+    }
+
     public static void main(String[] args) {
         Main m = new Main();
         if (m.isMaster()) {
