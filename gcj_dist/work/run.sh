98f1727a4a04d2e5e3d9bd2e77ffc8542513e435
toshif <tfukuzawa@live.jp>
2017-04-26 01:02:19 +0100

    mac

--- gcj_dist/work/run.sh
+++ gcj_dist/work/run.sh
@@ -2,7 +2,10 @@
 
 TARGET_MAIN_JAVA=../../src/template/Main.java
 INPUT_JAVA=oops.java
-INPUTS=$(ls inputs/oops*)
+INPUTS=(inputs/oops*)
+for in_file in "${INPUTS[@]}"; do
+    echo "input file = ${in_file} "
+done
 
 ulimit -n 10000
 NUM_OF_NODES=10
@@ -12,12 +15,12 @@ DCJ="${DCJ_DIR}/dcj.sh"
 
 cd tmp
 
-for in_file in $INPUTS; do
+for in_file in "${INPUTS[@]}"; do
     rm -f ../tmp/*
 
     echo "----- $in_file -----"
     cat $TARGET_MAIN_JAVA | egrep -v "^package .+;" > Main.java
-    cp ../$in_file $INPUT_JAVA
+    cp "../$in_file" $INPUT_JAVA
 
     $DCJ test --source Main.java --library $INPUT_JAVA --nodes $NUM_OF_NODES
 
