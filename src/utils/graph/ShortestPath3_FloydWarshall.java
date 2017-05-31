f5ad28b09ff27eb50754d1bf33e97c0032390ee8
toshif <tfukuzawa@live.jp>
2017-05-31 23:09:52 +0100

    bootcamp

--- src/utils/graph/ShortestPath3_FloydWarshall.java
+++ src/utils/graph/ShortestPath3_FloydWarshall.java
@@ -7,18 +7,28 @@ import java.util.Arrays;
  * <p/>
  * O( V^3 )
  * <p/>
- * Created by toshif on 5/28/14.
+ *
  */
 public class ShortestPath3_FloydWarshall {
 
-    static int N = 7;
+    private int N;
 
-    static int INF = 1_000_000_000;
+    public static long INF = 1_000_000_000_000_000_000L;
 
     // 各頂点間の最短距離
-    int d[][] = new int[N][N];
+    public long d[][];
 
-    void floydWarshall() {
+    public ShortestPath3_FloydWarshall(int N) {
+        this.N = N;
+        this.d = new long[N][N];
+
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(d[i], INF);
+            d[i][i] = 0;
+        }
+    }
+
+    public void floydWarshall() {
         for (int k = 0; k < N; k++) {
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
@@ -28,21 +38,16 @@ public class ShortestPath3_FloydWarshall {
         }
     }
 
+    public void addEdge(int from, int to, long cost) {
+        d[from][to] = cost;
+    }
+
     public static void main(String[] args) {
-        ShortestPath3_FloydWarshall fw = new ShortestPath3_FloydWarshall();
+        ShortestPath3_FloydWarshall fw = new ShortestPath3_FloydWarshall(7);
 
         // Undirected Graph
         String costs = "A,B,2 A,C,5 B,C,4 B,D,6 B,E,10 C,D,2 D,F,1 E,F,3 E,G,5 F,G,9";
 
-        // 存在しない path は INF
-        // d[i][i] = 0
-        int[][] d = fw.d;
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(d[i], INF);
-            d[i][i] = 0;
-        }
-
-        // 存在する path は cost を更新する
         String[] costE = costs.split(" ");
         for (int i = 0; i < costE.length; i++) {
             String[] c = costE[i].split(",");
@@ -51,8 +56,8 @@ public class ShortestPath3_FloydWarshall {
             int p1 = c[1].charAt(0) - 'A';
             int cs = Integer.parseInt(c[2]);
 
-            d[p0][p1] = cs;
-            d[p1][p0] = cs;
+            fw.addEdge(p0, p1, cs);
+            fw.addEdge(p1, p0, cs);
         }
 
         // 解は A -> c -> D -> F -> E -> G で 16
@@ -60,10 +65,8 @@ public class ShortestPath3_FloydWarshall {
 
         System.out.println("floyd warshall");
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < 7; i++) {
             System.out.println(Arrays.toString(fw.d[i]));
         }
-
-
     }
 }
