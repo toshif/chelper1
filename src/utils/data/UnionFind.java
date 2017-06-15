4e6318bd80cb7f2572528c33c00f3328d77901e9
toshif <tfukuzawa@live.jp>
2017-06-15 16:53:08 +0100

    mac

--- src/utils/data/UnionFind.java
+++ src/utils/data/UnionFind.java
@@ -25,7 +25,7 @@ public class UnionFind {
 
     public static void main(String[] args) {
         int N = 10;
-        UF uf = new UF(N);
+        UnionFind uf = new UnionFind(N);
         uf.union(0, 3);
         uf.union(0, 5);
         uf.union(0, 6);
@@ -49,50 +49,48 @@ public class UnionFind {
         System.out.printf("%s and %s connected ? : %s\n", 0, 7, uf.connected(0, 7));
     }
 
-    static class UF {
-        private int[] id;     // id[i] = parent of i
-        private int[] rank;   // rank[i] = rank of subtree rooted at i
-        private int count;    // number of subsets
+    private int[] id;     // id[i] = parent of i
+    private int[] rank;   // rank[i] = rank of subtree rooted at i
+    private int count;    // number of subsets
 
-        public UF(int N) {
-            count = N;
-            id = new int[N];
-            rank = new int[N];
-            for (int i = 0; i < N; i++) {
-                id[i] = i;
-                rank[i] = 0;
-            }
+    public UnionFind(int N) {
+        count = N;
+        id = new int[N];
+        rank = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    public int find(int p) {
+        while (p != id[p]) {
+            id[p] = id[id[p]]; // path compression by halving
+            p = id[p];
+        }
+        return p;
+    }
+
+    public void union(int p, int q) {
+        int i = find(p);
+        int j = find(q);
+        if (i == j) return;
+
+        // make root of smaller rank point to root of larger rank
+        if (rank[i] < rank[j]) {
+            id[i] = j;
+        } else if (rank[i] > rank[j]) {
+            id[j] = i;
+        } else {
+            id[j] = i;
+            rank[i]++;
         }
 
-        public int find(int p) {
-            while (p != id[p]) {
-                id[p] = id[id[p]]; // path compression by halving
-                p = id[p];
-            }
-            return p;
-        }
+        count--;
+    }
 
-        public void union(int p, int q) {
-            int i = find(p);
-            int j = find(q);
-            if (i == j) return;
-
-            // make root of smaller rank point to root of larger rank
-            if (rank[i] < rank[j]) {
-                id[i] = j;
-            } else if (rank[i] > rank[j]) {
-                id[j] = i;
-            } else {
-                id[j] = i;
-                rank[i]++;
-            }
-
-            count--;
-        }
-
-        public boolean connected(int p, int q) {
-            return find(p) == find(q);
-        }
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
     }
 
 }
